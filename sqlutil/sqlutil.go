b62c52e928210a3d28d6293c0c6712edf0cb6739
ot07 <ot07neoki@gmail.com>
2023-12-10 22:33:12 +0900

    feat: whereInを要素数0のスライスにも対応させる

--- sqlutil/sqlutil.go
+++ sqlutil/sqlutil.go
@@ -6,13 +6,17 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
-func whereIn[T any](selectFn func(dest any, query string, args ...any) error, query string, args ...any) ([]T, error) {
-	var result []T
+func whereIn[T any, S any](selectFn func(dest any, query string, args ...any) error, query string, args []S) ([]T, error) {
+	if len(args) == 0 {
+		return []T{}, nil
+	}
 
-	_query, _args, err := sqlx.In(query, args...)
+	_query, _args, err := sqlx.In(query, []any{args}...)
 	if err != nil {
 		return []T{}, err
 	}
+
+	var result []T
 
 	err = selectFn(&result, _query, _args...)
 	if err != nil {
@@ -22,26 +26,26 @@ func whereIn[T any](selectFn func(dest any, query string, args ...any) error, qu
 	return result, nil
 }
 
-func WhereIn[T any](db *sqlx.DB, query string, args ...any) ([]T, error) {
-	return whereIn[T](db.Select, query, args...)
+func WhereIn[T any, S any](db *sqlx.DB, query string, args []S) ([]T, error) {
+	return whereIn[T](db.Select, query, args)
 }
 
-func TxWhereIn[T any](tx *sqlx.Tx, query string, args ...any) ([]T, error) {
-	return whereIn[T](tx.Select, query, args...)
+func TxWhereIn[T any, S any](tx *sqlx.Tx, query string, args []S) ([]T, error) {
+	return whereIn[T](tx.Select, query, args)
 }
 
-func WhereInContext[T any](ctx context.Context, db *sqlx.DB, query string, args ...any) ([]T, error) {
+func WhereInContext[T any, S any](ctx context.Context, db *sqlx.DB, query string, args []S) ([]T, error) {
 	selectFn := func(dest any, query string, args ...any) error {
 		return db.SelectContext(ctx, dest, query, args...)
 	}
 
-	return whereIn[T](selectFn, query, args...)
+	return whereIn[T](selectFn, query, args)
 }
 
-func TxWhereInContext[T any](ctx context.Context, tx *sqlx.Tx, query string, args ...any) ([]T, error) {
+func TxWhereInContext[T any, S any](ctx context.Context, tx *sqlx.Tx, query string, args []S) ([]T, error) {
 	selectFn := func(dest any, query string, args ...any) error {
 		return tx.SelectContext(ctx, dest, query, args...)
 	}
 
-	return whereIn[T](selectFn, query, args...)
+	return whereIn[T](selectFn, query, args)
 }
